6941204dfd92009d163c57d10d4d34641af0ec50
Levi Wallace <guysmiley.levi@gmail.com>
2020-11-26 01:44:50 -0800

    (+)fixed some rotations, and added some other stuff

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,14 +6,30 @@ public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<Transform> ts;
+    [Range(0, 1)]
+    public float speed = 0.01f;
+
+    private Vector3 firstPos;
+
     int idx = 0;
 
+    void Start()
+    {
+        firstPos = transform.position;
+        Init();
+    }
+
+    public void Init()
+    {
+        transform.position = firstPos;
+        idx = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, ts[idx].position, 0.01f);
-        if (Vector3.Distance(transform.position, ts[idx].position) < 0.1f && idx < ts.Count)
+        transform.position = Vector3.MoveTowards(transform.position, ts[idx].position, speed);
+        if (Vector3.Distance(transform.position, ts[idx].position) < 0.1f && idx < ts.Count-1)
         {
             idx++;
         }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,30 +7,50 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     public Collider2D col;
+    public GameObject light;
 
     float speed = 5f;
-    float jumpspeed = 400f;
+    public float jumpspeed = 400f;
     Rigidbody2D rb;
     BoxCollider2D bc;
+    private Vector3 checkpointPos;
     public bool isGrounded = true;
+
+    public float jumpTimer = 1.0f;
+
 
     void Start()
     {
+        checkpointPos = Vector3.zero;
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
     }
 
+    void Init()
+    {
+        transform.position = checkpointPos;
+        transform.localRotation = new Quaternion(0, 0, 0, 0);
+        rb.velocity = Vector2.zero;
+        light.GetComponent<Movement>().Init();
 
+    }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(transform.position + (Vector3.down / 3.5f), 0.4f, LayerMask.GetMask("TileMap")) != null;
-        
         Vector2 movement = new Vector2();
-        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        if (isGrounded && Input.GetKey(KeyCode.W))
         {
-            movement += Vector2.up * jumpspeed;
+            jumpTimer += 0.01f;
+            jumpTimer = Mathf.Clamp(jumpTimer, 0, .25f);
+            transform.localScale = new Vector3(1, 1 - jumpTimer, 1);
+        }
+        if (isGrounded && Input.GetKeyUp(KeyCode.W))
+        {
+            movement += Vector2.up * jumpspeed * jumpTimer * 5;
+            transform.localScale = new Vector3(1, jumpTimer, 1);
+            jumpTimer = 1.0f;
             isGrounded = false;
         }
         if (Input.GetKey(KeyCode.A))
@@ -43,17 +63,28 @@ public class PlayerMovement : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.S))
         {
-            movement += Vector2.down * speed;
+            movement += Vector2.down * speed * 0.2f;
         }
         rb.AddForce(movement);
         if (!isGrounded)
         {
-            transform.Rotate(new Vector3(0, 0, -rb.velocity.x/5));
+            transform.Rotate(new Vector3(0, 0, rb.velocity.y/20 * Mathf.Sign(rb.velocity.x)));
         }
+        if (Vector3.Distance(light.transform.position, transform.position) > 15)
+        {
+            Init();
+        }
+        transform.localScale = Vector3.MoveTowards(transform.localScale, new Vector3(1, 1, 1), 0.1f);
+        /*
         transform.localScale =
-            new Vector3(1 + (rb.velocity.y * jumpspeed / 10000) * Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad),
-                        1 + (rb.velocity.y * jumpspeed / 10000) * Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad),
+            new Vector3(Mathf.Clamp(1 + rb.velocity.y * jumpspeed / 10000 *
+                            Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad),
+                            0.5f, 1.5f),
+                        Mathf.Clamp(1 + rb.velocity.y * jumpspeed / 10000 *
+                        Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad),
+                            0.5f, 1.5f),
                         1);
+         */
     }
 
 }
